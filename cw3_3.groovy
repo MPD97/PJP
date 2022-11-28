7416d44b64b7eadf45d6584c3b9efb5ecd527a00
MPD97 <mateuszpracadom@gmail.com>
2022-11-28 23:09:14 +0100

    Poprawka zadania 3

--- cw3_3.groovy
+++ cw3_3.groovy
@@ -2,25 +2,31 @@ import java.text.ParseException
 import java.text.SimpleDateFormat
 class dateParser{
 
-	static void isDateValid(String date){
-		def pattern = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/
+	static void isDateValid(String dates){
+		def pattern = /[0-9]{4}-[0-9]{2}-[0-9]{2}/
 
-		if(date.matches(pattern))
-		{
+		def matches = dates =~ pattern
+		
+		if(matches.size() == 0){
+			println "$dates nie posiada prawidłowych dat."
+			return
+		}
+		
+		matches.findAll({ 
 			try {
 				def df = new SimpleDateFormat("yyyy-MM-dd")
 				df.setLenient(false)
-				df.parse(date)
+				df.parse(it)
 				
-				println "$date jest prawidłową datą."
+				println "$it jest prawidłową datą."
 				return
 			} catch(ParseException e)
 			{
 			
 			}
-			
-			println "$date nie jest prawidłową datą."
-		}
+			println "$it nie jest prawidłową datą."
+		})
+		
 	}
 	
 	static void main(String[] args){
@@ -29,10 +35,13 @@ class dateParser{
 		def invalidDate1 = "2022-12-32"
 		def invalidDate2 = "2020-13-01"
 
+		def textWithDates = "Ala ma kota. 2022-03-19 to 19 dzień marca. Data 2022-14-45 jest nieprawidłowa."
 
 		isDateValid(validDate1)
 		isDateValid(validDate2)
 		isDateValid(invalidDate1)
 		isDateValid(invalidDate2)
+		
+		isDateValid(textWithDates)
 	}
 }
